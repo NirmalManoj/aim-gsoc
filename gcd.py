033f4db839e070631d375b027575eba7bac91688
Sagar <chsagar946@gmail.com>
2023-02-11 00:31:13 +0530

    Fixing Gcd
    Error In The Code , The Code For Addition Of Two Numbers

--- gcd.py
+++ gcd.py
@@ -2,6 +2,9 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-print("The GCD of", num1, "and", num2, "is", result)
+print("The GCD of", num1, "and", num2, "is", gcd(num1, num2))

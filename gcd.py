b2306697c499c9e25a7812c28db1e6e0a2ef9500
Jaswanth <jaswanth.aitha21@gmail.com>
2023-02-11 17:24:22 +0530

    written code to find gcd of two numbers

--- gcd.py
+++ gcd.py
@@ -2,6 +2,10 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+def hcf(num1,num2):
+    if(num2==0):
+        return num1
+    else:
+        return hcf(num2,num1%num2)
 
-print("The GCD of", num1, "and", num2, "is", result)
+print("The GCD of", num1, "and", num2, "is", hcf(num1,num2))

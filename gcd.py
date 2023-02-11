52726d5e6765a6792c17d0480edd79140fadba70
Dhanumjai <dhanudhanumjai444@gmail.com>
2023-02-11 11:38:10 +0530

    fix-gcd
    i have used for loop to iterate over the numbers from 1 to num1 to check which number is divisible by both num1 and num2

--- gcd.py
+++ gcd.py
@@ -2,6 +2,10 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+result = 0
+for i in range(1,num1):
+    if num2%i == 0:
+        if num1%i == 0:
+            result = i
 
 print("The GCD of", num1, "and", num2, "is", result)

193b41d4282d6edc70009403645f393fcd5ead57
Rutvik Pradyumna <pradyumnarutvik@gmail.com>
2023-02-14 13:35:40 +0530

    Updated addition.py gcd.py and simple_interest.py file accordingly to get correct result

--- gcd.py
+++ gcd.py
@@ -1,7 +1,13 @@
+def gcd(a,b):
+    if(b==0):
+        return a
+    return gcd(b,a%b)
+
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+result = gcd(num1,num2)
 
 print("The GCD of", num1, "and", num2, "is", result)

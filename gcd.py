d360fc42397ed5bd33432f1b081caa1ddd62ae4a
Hasinichaithanya <hasinichaithanya04@gmail.com>
2023-02-14 16:06:29 +0530

    Fixed the gcd calculation

--- gcd.py
+++ gcd.py
@@ -1,7 +1,17 @@
+def gcd(a,b):
+    result = min(a,b)
+    while result:
+        if a%result==0 and b%result ==0:
+            break
+        result-=1
+        
+    return result
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+gcd = gcd(num1,num2)
 
-print("The GCD of", num1, "and", num2, "is", result)
+print("The GCD of", num1, "and", num2, "is", gcd)
+
+

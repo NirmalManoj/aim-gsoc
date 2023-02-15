a508f5e3c24fdc334544d4719bdeb408df598b63
himanshu22500 <himanshu22500@gmail.com>
2023-02-15 16:47:36 +0530

    added function to calculate gcd

--- gcd.py
+++ gcd.py
@@ -2,6 +2,13 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+
+def gcd(num1, num2):
+    if num1 == 0:
+        return num2
+
+    return gcd(num2 % num1, num1)
+
+result = gcd(num1,num2)
 
 print("The GCD of", num1, "and", num2, "is", result)

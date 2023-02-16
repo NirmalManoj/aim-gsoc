d45abfcc47491a39ac1100d6687dd879e988f394
Jaswanth <jassumantri@gmail.com>
2023-02-16 16:16:35 +0530

    Find simple Interest
    Put int keyword in principle and time and the interest
    divided by 100

--- simple_interest.py
+++ simple_interest.py
@@ -1,8 +1,8 @@
-principal = float(input("Enter the principal amount: "))
+principal = int(input("Enter the principal amount: "))
 rate = float(input("Enter the interest rate (in percentage (1-100)): "))
-time = float(input("Enter the number of years for the investment: "))
+time = int(input("Enter the number of years for the investment: "))
 
 # Simple Interest Calculation
-interest = (principal * rate * time)
+interest = (principal * rate * time)/100
 
 print("The simple interest for an investment of", principal, "at", rate, "% for", time, "years is:", interest)

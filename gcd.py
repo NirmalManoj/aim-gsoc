59dcc74bb02080a0f1a9d77547f042bc7f59126f
Jaswanth <jassumantri@gmail.com>
2023-02-16 15:45:35 +0530

    Writing code to find greatest common divisor
    Take the small number in the given numbers by using if condition and run the for loop and check num1 and num2 are divisible by i then print result

--- gcd.py
+++ gcd.py
@@ -1,7 +1,13 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-# greatest common divisor
-result = num1 + num2
+if num1 < num2:
+    smaller = num1
+else:
+    smaller = num2
+
+for i in range(1,smaller+1):
+    if num1%i==0 and num2%i==0:
+        result = i
 
 print("The GCD of", num1, "and", num2, "is", result)

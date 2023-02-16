05793b00f1e526929beba9856c31870713b95609
arvnd <aravindnethi1632@gmail.com>
2023-02-16 19:51:15 +0530

    Updated by writing a function of gcd using while loop

--- gcd.py
+++ gcd.py
@@ -1,7 +1,14 @@
+# greatest common divisor
+
+def gcd(num1,num2):
+    while(num2):
+       num1, num2 = num2, num1 % num2
+    return abs(num1)
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-# greatest common divisor
-result = num1 + num2
+
+result=gcd(num1,num2)
 
 print("The GCD of", num1, "and", num2, "is", result)

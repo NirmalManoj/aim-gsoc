868e515a30ad75f7c92d147e67350144b8d22e63
komalpasumarthy <92156651+komalpasumarthy@users.noreply.github.com>
2023-02-18 04:39:48 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -2,6 +2,22 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
-
+def gcd(a, b):
+ 
+    # Everything divides 0
+    if (a == 0):
+        return b
+    if (b == 0):
+        return a
+ 
+    # base case
+    if (a == b):
+        return a
+ 
+    # a is greater
+    if (a > b):
+        return gcd(a-b, b)
+    return gcd(a, b-a)
+  
+result = gcd(num1, num2)
 print("The GCD of", num1, "and", num2, "is", result)

acf424b8cbf3f438915685f69919b8e70e4cf7ef
Bhavana Maheshwaram <123150426+bhavana-maheshwaram@users.noreply.github.com>
2023-02-18 02:27:27 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -2,6 +2,8 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+while num2:
+    num1, num2 = num2, num1 % num2
+result = num1
 
 print("The GCD of", num1, "and", num2, "is", result)

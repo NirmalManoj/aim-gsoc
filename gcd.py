0280a338273d844d8d732e730e8058f1f560b7a1
Avinash mattupalli <msvavinash@gmail.com>
2023-02-18 23:25:28 +0530

    fixxadd

--- gcd.py
+++ gcd.py
@@ -1,7 +1,13 @@
+def gcd(num1,num2):
+    if num2==0:
+        return abs(num1)
+    else:
+        return gcd(num2,num1%num2)
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+result = gcd(num1,num2)
 
 print("The GCD of", num1, "and", num2, "is", result)

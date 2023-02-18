42bb1537ca9c45f2f8896652e92f093f7f6791fb
Bala <vbkrao2020@gmail.com>
2023-02-18 16:58:08 +0530

    Fixed-gcd-problem
    Code was performing addition instead of  GCD operation

--- gcd.py
+++ gcd.py
@@ -2,6 +2,14 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+if num1==0:
+    result=num2
+
+else:    
+    while(num2 != 0):
+        temp = num2
+        num2 = num1 % num2
+        num1 = temp
+    result = num1   
 
 print("The GCD of", num1, "and", num2, "is", result)

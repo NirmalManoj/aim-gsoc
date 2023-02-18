9880201c19345eacc630256f0edf15938b325fd7
Akash <akashpadala197@gmail.com>
2023-02-18 22:53:18 +0530

    Fixed GCD
    Addition is done instead of calculating GCD

--- gcd.py
+++ gcd.py
@@ -1,7 +1,13 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
+if num1>num2:
+    small = num2
+else:
+    small = num1
 # greatest common divisor
-result = num1 + num2
+for i in range(1, num2+1):
+    if((num1%i == 0) and (num2%i == 0)):
+        result = i
 
 print("The GCD of", num1, "and", num2, "is", result)

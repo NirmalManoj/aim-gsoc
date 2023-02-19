685ed827623e4b89f173919091523e5f06696288
lrtraviteja <lrtraviteja@gmail.com>
2023-02-19 11:53:30 +0530

    Fixed GCD of two numbers
    Instead of getting the GCD we are getting sum of given two numbers hence used the GCD function to solve the issue.

--- gcd.py
+++ gcd.py
@@ -2,6 +2,13 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+def gcd(a,b):
+    while (b != 0):
+        rem = a % b
+        a = b
+        b = rem
+    return a
+    
+result = gcd(num1, num2)
 
 print("The GCD of", num1, "and", num2, "is", result)

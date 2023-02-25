2ac1fccefd77745a4d0f4cf9adfc53740e35ef03
ashrithchirala <ashrithchirala@gmail.com>
2023-02-25 22:59:56 +0530

    fixed gcd

--- gcd.py
+++ gcd.py
@@ -1,7 +1,9 @@
+import math
+
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+result = math.gcd(num1,num2)
 
 print("The GCD of", num1, "and", num2, "is", result)

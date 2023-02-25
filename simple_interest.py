78d1b78f96431fee9b2fa64329cc3c2e8aa2b15a
CHARANCHARY <charanchary999@gmail.com>
2023-02-25 20:40:40 +0530

    simple intrest calculater has been done

--- simple_interest.py
+++ simple_interest.py
@@ -3,6 +3,6 @@ rate = float(input("Enter the interest rate (in percentage (1-100)): "))
 time = float(input("Enter the number of years for the investment: "))
 
 # Simple Interest Calculation
-interest = (principal * rate * time)
+interest = (principal * rate * time)/100
 
 print("The simple interest for an investment of", principal, "at", rate, "% for", time, "years is:", interest)

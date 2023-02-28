aa1a68950305f21f77e463c9c1c1caa77ccc874c
Srimani <srimani0723@gmail.com>
2023-02-28 20:26:19 +0530

    Rate Percentage
    Rate is not converted to % which is divided by 100

--- simple_interest.py
+++ simple_interest.py
@@ -2,6 +2,8 @@ principal = float(input("Enter the principal amount: "))
 rate = float(input("Enter the interest rate (in percentage (1-100)): "))
 time = float(input("Enter the number of years for the investment: "))
 
+rate = rate/100
+
 # Simple Interest Calculation
 interest = (principal * rate * time)
 

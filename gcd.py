d4ce3045e10611895c575e093d32f8a94c837d85
Ganeshganap <ganeshganap3@gmail.com>
2023-09-10 20:29:58 +0600

    Solved Greatest Common Divisor

--- gcd.py
+++ gcd.py
@@ -1,7 +1,18 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+def get_gcd(num_1, num_2):
+	if(num_1 == num_2) or (num_2 == 0) :
+		return num_1
+	elif(num_1  == 0):
+		return num_2
+	else:
+		if(num_1 > num_2) :
+			return get_gcd(num_1 - num_2, num_2)
+		else:
+			return get_gcd(num_1, num_2 - num_1)
 
 # greatest common divisor
-result = num1 + num2
+result = get_gcd(num_1, num_2)
 
-print("The GCD of", num1, "and", num2, "is", result)
+print("The GCD of", num_1, "and", num_2, "is", result)
